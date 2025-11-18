9c9c64adfdd8922b0807a84755bfe33c9bc428b1
Paulo Silva <97074193+PauloTheLord@users.noreply.github.com>
2025-11-18 20:55:01 -0300

    💎style: Chatbot button

--- frontend/vite-project/src/pages/App.tsx
+++ frontend/vite-project/src/pages/App.tsx
@@ -182,6 +182,13 @@ export default function App() {
       </div>
 
     </main>
+    <Link to="/doceAI">
+      <button className="chat-button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="white" className="bi bi-chat-left-fill" viewBox="0 0 16 16">
+  <path d="M2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+</svg>
+      </button>
+    </Link>
     </div>
   )
 }
